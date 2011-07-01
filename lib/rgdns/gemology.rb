cc246a89d2644f5ad51a368b93e00b6493e74da0
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-07-01 15:35:05 +0200

    make it a commandline to dump the specs

--- lib/rgdns/gemology.rb
+++ lib/rgdns/gemology.rb
@@ -1,11 +1,10 @@
-require 'sequel'
-require 'rubygems/specification'
+require 'sequel';
 module Rgdns
   class Gemology
     def initialize( sequel_options )
       @db = Sequel.connect( *sequel_options )
     end
-   
+
     def specifications
       @specifications ||= fetch_specifications
     end
@@ -14,9 +13,24 @@ module Rgdns
       sql = "SELECT specification FROM gem_version_raw_specifications"
       specs = []
       @db.fetch( sql ) do |r|
-        specs << eval( r )
+        specs << r[:specification]
       end
       return specs
     end
   end
 end
+
+if $0 == __FILE__ then
+  uuid = [ `uuidgen -r`.strip, `uuidgen -r`.strip ]
+  delimiter = uuid.join(':')
+  g = Rgdns::Gemology.new( ARGV.shift )
+  $stdout.puts delimiter
+  count = 0
+  g.specifications.each do |spec_text|
+    $stdout.puts spec_text
+    $stdout.puts delimiter
+    count += 1
+    $stderr.puts count if count % 10000 == 0
+  end
+end
+
