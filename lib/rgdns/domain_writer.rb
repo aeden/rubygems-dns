77e2940b247328bcfdc409e7db20f27cddf3463c
Anthony Eden <anthonyeden@gmail.com>
2011-07-01 15:41:33 +0200

    write to the db

--- lib/rgdns/domain_writer.rb
+++ lib/rgdns/domain_writer.rb
@@ -1,26 +1,101 @@
 module Rgdns
   class DomainWriter
+  
     def write(specification)
-      build_records(specification)
+      dn = dn(specification)
+      
+      domain_id = upsert_domain(dn)
+      upsert_soa_record(domain_id, dn)
+      upsert_ns_records(domain_id, dn)
+
+      build_records(domain_id, specification)
+      #records = build_records(specification)
+      #records.each do |record|
+        #records_table.insert(
+          #:domain_id => domain_id,
+          #:name => record[0],
+          #:type => record[1],
+          #:ttl => record[2],
+          #:content => record[3],
+          #:change_date => Time.now.to_i
+        #)
+      #end
     end
 
-    def fqdn(specification)
-      @fqdn ||= name_version_to_qname(specification.name, specification.version)
+    # Insert or update the domain record and return the domain id.
+    def upsert_domain(dn)
+      row = domains_table.filter(:name => dn).first
+      unless row && domain_id = row[:id]
+        domains_table.insert(:name => dn, :type => 'NATIVE')
+        domain_id = domains_table.filter(:name => dn).first[:id]
+      end
+      domain_id
+    end
+    protected :upsert_domain
+
+    def upsert_soa_record(domain_id, dn)
+      soa_record = records_table.filter(:domain_id => domain_id, :name => dn, :type => "SOA").first 
+      insert_record(domain_id, dn, "SOA", 86400, soa_content) unless soa_record
+    end
+    protected :upsert_soa_record
+
+    def soa_content
+      "ns1.rubygems.org admin@rubygems.org #{Time.now.strftime("%Y%m%d%H%M01")} 86400 7200 604800 300"
+    end
+    private :soa_content
+
+    def upsert_ns_records(domain_id, dn)
+      %w(ns1.rubygems.org ns2.rubygems.org).each do |name|
+        ns_record = records_table.filter(:domain_id => domain_id, :name => dn, :type => "NS", :content => name).first
+        insert_record(domain_id, dn, "NS", 3600, name) unless ns_record
+      end
+    end
+    protected :upsert_ns_records
+
+    def upsert_latest_ptr(domain_id, fqdn_latest, fqdn)
+      latest_ptr_record = records_table.filter(:domain_id => domain_id, :name => fqdn_latest, :type => "CNAME").first
+      if latest_ptr_record
+        records_table.filter(:id => latest_ptr_record[:id]).update(:content => fqdn)
+      else
+        insert_record(domain_id, fqdn_latest, "CNAME", 600, fqdn) 
+      end
+    end
+    protected :upsert_latest_ptr
+
+    def upsert_partial_versions(domain_id, specification)
+      fqdn = fqdn(specification)
+      vr = specification.version.to_s.split(".").reverse
+      while vr.shift
+        break if vr.empty?
+        name = name_version_to_qname(specification.name, vr)
+        record = records_table.filter(:domain_id => domain_id, :name => name).first
+        if record
+          records_table.filter(:id => record[:id]).update(:content => fqdn, :change_date => Time.now.to_i)
+        else
+          insert_record(domain_id, name, "CNAME", 600, fqdn)
+        end
+      end
     end
 
-    def fqdn_devel(specification)
-      @fqdn_devel ||= name_version_to_qname(specification.name + "-devel", specification.version)
+    def insert_record(domain_id, name, type, ttl, content)
+      records_table.insert(
+        :domain_id => domain_id,
+        :name => name,
+        :type => type, 
+        :ttl => ttl, 
+        :content => content,
+        :change_date => Time.now.to_i
+      )
     end
+    private :insert_record
 
-    def dn(specification)
-      @dn ||= name_to_domain(specification.name)
+    def insert_record_if_new(domain_id, name, type, ttl, content)
+      record = records_table.filter(:domain_id => domain_id, :name => name, :type => type, :content => content).first
+      insert_record(domain_id, name, type, ttl, content) unless record
     end
+    private :insert_record_if_new
 
-    def fqdn_latest(specification)
-      @fqdn_latest ||= ["latest", fqdn(specification)].join(".")
-    end
-
-    def build_records(specification)
+    def build_records(domain_id, specification)
       records = []
 
       dn = dn(specification)
@@ -28,30 +103,19 @@ module Rgdns
       fqdn_latest = fqdn_latest(specification)
       fqdn_devel = fqdn_devel(specification)
 
-      # SOA record
-      records << [dn, "SOA", 86400, "ns1.rubygems.org admin@rubygems.org #{Time.now.strftime("%Y%m%d%H%M01")} 86400 7200 604800 300"]
-
-      # NS records
-      records << [dn, "NS", 3600, "ns1.rubygems.org"]
-      records << [dn, "NS", 3600, "ns2.rubygems.org"]
-
       # CNAME records
-      records << [fqdn_latest, "CNAME", 600, fqdn]
-      vr = specification.version.to_s.split(".").reverse
-      while vr.shift
-        break if vr.empty?
-        records << [name_version_to_qname(specification.name, vr), "CNAME", 600, fqdn]
-      end
+      upsert_latest_ptr(domain_id, fqdn_latest, fqdn)
+      upsert_partial_versions(domain_id, specification)
 
       # PTR record for latest version
-      records << [dn, "PTR", 86400, fqdn]
+      insert_record_if_new(domain_id, dn, "PTR", 86400, fqdn)
 
       # PTR records for dependencies
       specification.runtime_dependencies.each do |dep|
-         records << [fqdn, "PTR", 86400, dependency_to_qname(dep)]
+        insert_record_if_new(domain_id, fqdn, "PTR", 86400, dependency_to_qname(dep))
       end
       specification.development_dependencies.each do |dep|
-         records << [fqdn_devel, "PTR", 86400, dependency_to_qname(dep)]
+        insert_record_if_new(domain_id, fqdn_devel, "PTR", 86400, dependency_to_qname(dep))
       end
 
       records
@@ -83,7 +147,6 @@ module Rgdns
       else
         raise "Can't handle versin #{version}"
       end
-
     end
 
     def base_name
@@ -103,5 +166,39 @@ module Rgdns
       end
       vr
     end
+
+    private
+
+    def db_url
+      db_url = ENV['DB_URL'] || 'mysql://root@localhost/powerdns'
+    end
+    def db 
+      @db ||= Sequel.connect(db_url)
+    end
+
+    def domains_table
+      @domains_table ||= db[:domains]
+    end
+
+    def records_table
+      @records_table ||= db[:records]
+    end
+
+    def fqdn(specification)
+      @fqdn ||= name_version_to_qname(specification.name, specification.version)
+    end
+
+    def fqdn_devel(specification)
+      @fqdn_devel ||= name_version_to_qname(specification.name + "-devel", specification.version)
+    end
+
+    def dn(specification)
+      @dn ||= name_to_domain(specification.name)
+    end
+
+    def fqdn_latest(specification)
+      @fqdn_latest ||= ["latest", fqdn(specification)].join(".")
+    end
+
   end
 end
